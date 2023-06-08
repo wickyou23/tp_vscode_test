de818287c2bfcdf97c337d1623b48ca56430cf01
thang.phung <thang.phung@veriksystems.com>
2023-06-08 16:30:48 +0700

    update code

--- constants.py
+++ constants.py
@@ -1,7 +1,15 @@
-#Constant for searching part number feature
-USER_DEFAULT = ""
-PASSWORD_DEFAULT = ""
-CSV_FILE_NAME = ""
-URL_DEFAULT = ""
+import private_constant as inner
+
+#Searching part number feature
+USER_DEFAULT = inner.URL_DEFAULT
+PASSWORD_DEFAULT = inner.PASSWORD_DEFAULT
+CSV_FILE_NAME = inner.CSV_FILE_NAME
+URL_DEFAULT = inner.URL_DEFAULT
+
+#DB Connection
+DB_HOST = inner.DB_HOST
+DB_PORT = inner.DB_PORT
+DB_USER = inner.DB_USER
+DB_PASSWORD = inner.DB_PASSWORD
 
 #Others
